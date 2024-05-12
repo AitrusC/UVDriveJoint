db14b2a04e6fd29b3751e962878bc27b7bd2b340
AitrusC <127205557+AitrusC@users.noreply.github.com>
2024-05-12 21:34:08 +0800

    Delete driveF directory

--- driveF/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-# @FileName      :__init__
-# @Time          :2023/9/11 21:26
-# @Author        :AhrIlI
-# @Contact       :906629272@qq.com
-
-import driveFunction
-
-reload(driveFunction)
